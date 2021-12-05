27dd03a8e6a583209c1954e54e6d628602f16839
featrd3 <43852603+featrd3@users.noreply.github.com>
2021-12-05 18:04:14 +0100

    adding draw solving mechanic

--- scripts/CardGame/deck.js
+++ scripts/CardGame/deck.js
@@ -1,5 +1,7 @@
 const SUITS = ["♠","♥","♦","♣"]
-const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+//const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+const VALUES = ["A","2","3"]
 
 export default class Deck{
     constructor(cards = freshDeck()){
